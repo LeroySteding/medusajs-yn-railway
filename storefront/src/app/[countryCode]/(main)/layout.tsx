af51ad8c4a7e2eec94aed8aaaad5fef883a8a5d4
Leroy Steding <leroy@hifive.dev>
2025-02-03 13:56:03 +0100

    ADjusted thenme

--- storefront/src/app/[countryCode]/(main)/layout.tsx
+++ storefront/src/app/[countryCode]/(main)/layout.tsx
@@ -1,19 +1,24 @@
 import { Metadata } from "next"
 
 import Footer from "@modules/layout/templates/footer"
-import Nav from "@modules/layout/templates/nav"
 import { getBaseURL } from "@lib/util/env"
+import { SiteHeader } from "components/header/site-header"
+import { InstagramCarousel } from "components/instagram-carousel"
+import { listCategories } from "@lib/data/categories"
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
 }
 
 export default async function PageLayout(props: { children: React.ReactNode }) {
+  const categories = await listCategories()
+  
   return (
-    <>
-      <Nav />
+    <div className="flex flex-col min-h-screen justify-between">
+      <SiteHeader categories={categories} />
       {props.children}
+      <InstagramCarousel />
       <Footer />
-    </>
+    </div>
   )
 }
